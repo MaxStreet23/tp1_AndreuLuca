d572f0b8d2fbb3441b0be3a1a0219191c84fd393
PC <PC@MaxStreet-PC>
2024-04-19 18:43:20 -0300

    Se aplica un control al ingreso de datos del ejercicio 13

--- tp1_AndreuLuca/src/ar/edu/unju/fi/ejercicio13/Main.java
+++ tp1_AndreuLuca/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -4,9 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		int[] numeros=new int[8];
 		Scanner sc=new Scanner(System.in);
+		boolean ingresado=false;
 		for(int i=0;i<8;i++) {
-			System.out.print("Ingrese un numero "+(i+1)+"/"+"8"+": ");
-			numeros[i]=sc.nextInt();
+			while(ingresado==false){
+				System.out.print("Ingrese un numero "+(i+1)+"/"+"8"+": ");
+				try {
+					numeros[i]=Integer.parseInt(sc.nextLine());
+					ingresado=true;
+				}catch (NumberFormatException e) {
+					System.out.println("Ingrese un numero valido");
+				}
+			}
+			ingresado=false;
 		}
 		sc.close();
 		for(int i=0;i<8;i++) {
